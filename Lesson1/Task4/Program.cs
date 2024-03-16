27f9aca7d80260b8cb7022ba08d398e600c3e7f5
Kirill <korneeveckirill5@gmail.com>
2024-03-16 23:01:00 +0300

    add Lesson1:Task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 6;
+int b = 2;
+int c = 9;
+int d = 7;
+int e = 4;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+if (d > max)
+{
+    max = d;
+}
+
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
