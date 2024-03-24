a13dd32182648195dcb7e5d1b34cda2881f99dd9
Kirill <korneeveckirill5@gmail.com>
2024-03-24 17:00:21 +0300

    Task4:while, for, foreach

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Дано N гирь с разным весом в случайном порядке. Определть вес самой тяжёлой гири.
-
+int n = 7;
 int[] arr = { 5, 7, 1, 78, 34, 2690, 12 };
 int i = 1;
 int max = arr[0];
@@ -11,5 +11,25 @@ while (i < arr.Length)
         max = arr[i];
     }
     i++;
+}
+System.Console.WriteLine(max);
+
+max = arr[0];
+for(int j = 0; j < n; j++)
+{
+    if (arr[j] > max)
+    {
+        max = arr[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = arr[0];
+foreach(int e in arr)
+{
+    if (e > max)
+    {
+        max = e;
+    }
 }
 System.Console.WriteLine(max);
