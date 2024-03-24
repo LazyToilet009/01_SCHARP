8f1cb292e6768f8b1fa9825310cecbc923648189
Kirill <korneeveckirill5@gmail.com>
2024-03-24 15:44:27 +0300

    add Task2

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Создать массив и заполнить его целыми числами от 1 до N  (N - произвольное натуральноне число). Вывести элементы массива на экран.
+
+System.Console.Write("Введи целое натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int i = 0;
+int[] arr42 = new int[n];
+
+while (i < n)
+{
+    arr42[i] = i + 1;
+    System.Console.Write(arr42[i] + " ");
+    i++;
+}
