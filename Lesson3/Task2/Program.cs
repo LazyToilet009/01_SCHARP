f8319d3df2bf8134aa1087237d7e723bde80677e
Kirill <korneeveckirill5@gmail.com>
2024-03-25 23:54:28 +0300

    add Task2

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вывод на экран квадраты чисел от 1 до N
+
+void square(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        System.Console.Write(i * i + " ");
+        i++;
+    }
+}
+
+square(5);
+System.Console.WriteLine();
+square(10);
+System.Console.WriteLine();
+square(15);
